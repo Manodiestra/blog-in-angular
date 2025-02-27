b982dca18e9f54a93df0536b091e54af56af34e3
Manodiestra <52931506+Manodiestra@users.noreply.github.com>
2025-02-27 13:16:11 -0700

    create some blog files and a service file skeleton

---
+++ src/app/pages/add-edit-blog/add-edit-blog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-blog',
+  imports: [],
+  templateUrl: './add-edit-blog.component.html',
+  styleUrl: './add-edit-blog.component.css'
+})
+export class AddEditBlogComponent {
+
+}
