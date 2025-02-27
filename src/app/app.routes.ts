4b89efe0b6410b558e61c6de72b7755e9af8f8a6
Manodiestra <52931506+Manodiestra@users.noreply.github.com>
2025-02-27 13:36:35 -0700

    add http client and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogListComponent } from './pages/blog-list/blog-list.component';
+import { BlogDetailComponent } from './pages/blog-detail/blog-detail.component';
+import { AddEditBlogComponent } from './pages/add-edit-blog/add-edit-blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blogs', component: BlogListComponent },
+  { path: 'blogs/:id', component: BlogDetailComponent },
+  { path: 'add', component: AddEditBlogComponent },
+  { path: 'edit/:id', component: AddEditBlogComponent }
+];
