b93a55cc3ecf40c61a6c38223707e9486602bf44
Manodiestra <52931506+Manodiestra@users.noreply.github.com>
2025-02-27 16:23:38 -0700

    Set up the template for the main home page. Set up routing correctly.

--- src/app/pages/blog-list/blog-list.component.ts
+++ src/app/pages/blog-list/blog-list.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog-list',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.css']
 })
